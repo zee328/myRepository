e6cf43f8221e684d21092fd35368335ae55ad5c9
zee328 <153637763+zee328@users.noreply.github.com>
2024-11-25 20:32:29 +0000

    Add for-ofLoops.js file

---
+++ for-ofLoops.js
@@ -0,0 +1,10 @@
+const fruits = ['apple', 'banana', 'orange'];
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+const numbers = [1, 2, 3, 4, 5];
+
+for (let number of numbers) { // iterating over the numbers array
+  console.log(number + 1); // For each number, add 1 and log it
+}
