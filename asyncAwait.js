abbc2e423ec11b81d7b3027b605d557c32e2375b
zee328 <153637763+zee328@users.noreply.github.com>
2024-12-13 19:32:47 +0000

    Update asyncAwait.js

--- asyncAwait.js
+++ asyncAwait.js
@@ -13,3 +13,27 @@ async function main() {
 }
 
 main();
+
+
+//2,code:
+
+async function fetchUserData(){
+return new Promise(resolve=> {
+setTimeOut(()=> resolve ({user:'Mike' , balance:300}},2000);
+}};
+}
+async function fetchProductData(){
+return new Promise(resolve=> {
+setTimeout(()=> resolve ({product:'Laptop' , price:900}),2000);
+});
+}
+async function getData(){
+try{ 
+const [user,product]=await Promise.all([fetchUserData(), fetchProductData()]);
+console.log('user info:', user);
+console.log('Product Info:' product);
+} catch(error){
+console.log('Error:', error);
+}
+}
+getData();
